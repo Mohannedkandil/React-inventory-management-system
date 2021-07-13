dc321a4107ee7b9555fc543f8df45f1d8f6c7ddd
Mohannedkandil <mohanadmkandil@gmail.com>
2021-07-13 23:30:59 +0200

    Changed Info component from Function to Class based

--- src/info.js
+++ src/info.js
@@ -1,16 +1,20 @@
-export default  function Info(){
-    const title = "this is my title";
-    const showTitle = true;
+import React from "react";
 
-    if (showTitle){
-        return(
-            <div>
-                <h1>{title}</h1>
-                <p>Manage yout stuff.</p>
-            </div>
-        );
-        
-    }else {
-        return <p>Empty</p>
+class Info extends React.Component {
+    render (){
+        const title = "This is the title";
+        const showTitle = true;
+        if (showTitle){
+            return (
+                <div>
+                    <h1>{title}</h1>
+                    <p>Manage your stuff</p>
+                </div>
+            );
+        }else {
+            return <p>Empty</p>
+        }
     }
+
 }
+export default Info;
